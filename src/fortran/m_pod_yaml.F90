1118edcb82861a16ed7ca25c1686c1423dd5c686
John <john.donovan@ga.gov.au>
2021-06-17 05:53:22 +0000

    create output directory if not exist. If can't revert to cur dir

--- src/fortran/m_pod_yaml.F90
+++ src/fortran/m_pod_yaml.F90
@@ -99,7 +99,8 @@ module pod_yaml
 
    character(512) yml_orbit_filename, yml_ext_orbit_filename, yml_satsinex_filename, yml_leapsecond_filename, yml_eop_filename
    character(512) yml_gravity_filename, yml_ephemeris_header, yml_ephemeris_data_file, yml_ocean_tides_file, yml_erp_filename
-   character(512) yml_ic_filename, yml_output_dir
+   character(512) yml_ic_filename, yml_output_dir, cmd
+   logical        dir_exists
 
    integer*4 yml_orbit_steps, yml_orbit_points, yml_orbit_arc_determination, yml_orbit_arc_prediction, yml_orbit_arc_backwards
    integer*4 yml_ext_orbit_steps, yml_ext_orbit_points, yml_eop_option, yml_eop_int_points, yml_estimator_iterations
@@ -309,6 +310,16 @@ subroutine get_yaml(yaml_filepath)
    else
       ! if output directory not specified, default it to '.'
       yml_output_dir = pod_options_dict%get_string("output_directory", ".", my_error_p)
+      dir_exists = .true.
+      INQUIRE(file=yml_output_dir, exist=dir_exists)
+      if (.not. dir_exists) then
+          write(cmd, *) "mkdir -p ", yml_output_dir
+          call system(trim(cmd))
+          INQUIRE(file=yml_output_dir, exist=dir_exists)
+          if (.not. dir_exists) then
+              write (*,*) "could not create output directory ", yml_output_dir, ", reverting to '.'"
+          end if
+      end if
       yml_pod_mode = get_pod_mode(pod_options_dict, my_error)
       yml_ic_input_format = get_input_format(pod_options_dict, yml_ic_filename, my_error)
       yml_ic_input_refsys = get_input_refsys(pod_options_dict, my_error)
