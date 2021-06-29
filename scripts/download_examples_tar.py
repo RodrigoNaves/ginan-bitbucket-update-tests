574037c42a0ffd71bdf21572425bb7aebadaac6d
Bogdan <matviichukb@gmail.com>
2021-06-29 04:29:32 +0000

    updated tarball and download script

--- scripts/download_examples_tar.py
+++ scripts/download_examples_tar.py
@@ -6,7 +6,6 @@ import base64
 import hashlib
 
 def get_checksum_S3(path2file):
-    print('---computing md5 checksum---')
     with open(path2file,'rb') as file:
         filehash = hashlib.md5()
         while True:
@@ -22,20 +21,31 @@ def untar(file):
         print('Extracting {} to {}'.format(file,destpath))
         tar.extractall(path=destpath)
         
-def download_examples_tar(url,relpath = '../'):
+def download_examples_tar(url,relpath = '../examples/'):
     '''relpath configures output path relative to the sctipt location'''
     destfile = os.path.basename(url) #proc.tar.gz
     script_path = os.path.dirname(os.path.realpath(__file__))
     destfile = os.path.abspath(os.path.join(script_path,relpath,destfile))
+    # print(destfile)
     if os.path.exists(destfile):
         print('file found')
         with urllib.request.urlopen(url) as response:
             if response.status == 200:
-                if get_checksum_S3(destfile) == (response.getheader('x-amz-meta-md5checksum')).encode():
-                    print("MD5 verified, skipping the download step")
-                    untar(destfile)
+                print('server says OK')
+                
+                # print(f'md5 = {get_checksum_S3(destfile)}')
+                md5_checksum_response =  response.getheader('x-amz-meta-md5checksum')
+                if md5_checksum_response is not None: #md5 checksum exists on server
+                    print('---computing md5 checksum---')
+                    if get_checksum_S3(destfile) == md5_checksum_response.encode():
+                        print("MD5 verified, skipping the download step")
+                        untar(destfile)
+                    else:
+                        print("MD5 check failed, redownloading the file")
+                        if os.path.exists(destfile): os.remove(destfile)
                 else:
-                    print("MD5 check failed, redownloading the file")
+                    print('no checksum found -> force redownloading the file')
+                    print(destfile)
                     if os.path.exists(destfile): os.remove(destfile)
 
     if not os.path.exists(destfile):
@@ -45,5 +55,5 @@ def download_examples_tar(url,relpath = '../'):
                 with open(destfile, 'wb') as out_file: shutil.copyfileobj(response, out_file)
         untar(destfile)
 
-url='https://peanpod.s3-ap-southeast-2.amazonaws.com/pea/proc.tar.gz'
+url='https://peanpod.s3.ap-southeast-2.amazonaws.com/aux/examples_aux.tar.gz'
 download_examples_tar(url=url)
