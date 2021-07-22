379b39a298ece690dbdb950f2f053e12a591624f
jamizannat_ga <umma.zannat@ga.gov.au>
2021-07-22 05:26:36 +0000

    test out parallel tests

---
+++ aws/docker/run-tests-pea.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -euo pipefail
+
+# activate conda environment
+eval "$(/root/.miniconda3/bin/conda shell.bash hook)"
+conda activate gn37
+
+# download example tests
+cd /ginan
+python scripts/download_examples.py
+
+# run example tests
+cd /ginan/examples
+pea --config ex11_pea_pp_user_gps.yaml
+pea --config ex12_pea_pp_user_gnss.yaml
+pea --config ex13_pea_pp_user_gps_sf.yaml
+pea --config ex14_pea_pp_user_gnss_ar.yaml
+# TODO not working:
+# pea --config ex15_pea_rt_user_gnss_ar.yaml
+# TODO absent:
+# ex16
+pea --config ex17_pea_pp_netw_gnss_ar.yaml
+# TODO not working:
+# pea --config ex18_pea_rt_netw_gnss_ar.yaml
